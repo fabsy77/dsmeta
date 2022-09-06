1fb2f2fe1de3697f63c8e80ce8b58fbd74758f9c
fabsy77 <fabianete@hotmail.com>
2022-09-06 20:29:02 +0200

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
-import icon from '../../assets/img/notification-icon.svg'  
+import icon from '../../assets/img/notification-icon.svg';
+
 
 function NotificationButton() {
     return (
@@ -8,5 +9,5 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
 
