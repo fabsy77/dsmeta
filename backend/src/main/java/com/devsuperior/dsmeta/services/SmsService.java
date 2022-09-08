ae88ebcc4f53cc2ddbaf1339990063880081d0f3
fabsy77 <fabianete@hotmail.com>
2022-09-08 19:55:02 +0200

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -27,10 +27,9 @@ public class SmsService {
 	@Value("${twilio.phone.to}")
 	private String twilioPhoneTo;
 	
-	
 	@Autowired
 	private SaleRepository saleRepository;
-	
+
 	public void sendSms(Long saleId) {
 		
 		Sale sale = saleRepository.findById(saleId).get();
@@ -39,13 +38,13 @@ public class SmsService {
 		
 		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
 			    + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
-
+			    
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
 		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 
-		Message message = Message.creator(to, from, "msg").create();
+		Message message = Message.creator(to, from, msg).create();
 
 		System.out.println(message.getSid());
 	}
